41a5007c436ca2f567597520de087a836187690c
paulohunterd <paulohunterd@gmail.com>
2025-04-20 11:10:36 -0300

    Adicionando teste de produtos

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -11,12 +11,12 @@ describe('Funcionalidade Pré cadastro', () => {
     });
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(emailFaker); // Linha 10
+        cy.get('#reg_email').type(emailFaker);
         cy.get('#reg_password').type('!teste@teste$');
         cy.get(':nth-child(4) > .button').click();
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click();
-        cy.get('#account_first_name').type(nomeFaker); // Linha 14
-        cy.get('#account_last_name').type(sobrenomeFaker); // Linha 15
+        cy.get('#account_first_name').type(nomeFaker);
+        cy.get('#account_last_name').type(sobrenomeFaker);
         cy.get('.woocommerce-Button').click();
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.');
     });
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da loja', () => {
+        cy.get('[class="product-block grid"]')
+            // .first()
+            //.last()
+            //.eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+    });
+
+    it.only('Deve adicionar o produto ao carrinho', () => {
+        var quantidade = 2
+
+        cy.get('[class="product-block grid"]')
+            .contains('Aero Daily Fitness Tee').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Brown').click()
+
+        cy.get('.input-text').clear().type(quantidade)
+
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+    });
+
+});
